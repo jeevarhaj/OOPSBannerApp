dc5f23591a47833d03979cb45c14b4046c62b4a1
jeevarhaj <rhajjeeva@gmail.com>
2026-03-02 20:03:22 +0530

    UC6: Modular banner using helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,59 @@
 public class OOPSBannerApp {
 
+    // Pattern for letter O
+    public static String[] getOPattern() {
+        return new String[]{
+            " *** ",
+            "** **",
+            "** **",
+            "** **",
+            "** **",
+            "** **",
+            " *** "
+        };
+    }
+
+    // Pattern for letter P
+    public static String[] getPPattern() {
+        return new String[]{
+            "**** ",
+            "** **",
+            "** **",
+            "**** ",
+            "**   ",
+            "**   ",
+            "**   "
+        };
+    }
+
+    // Pattern for letter S
+    public static String[] getSPattern() {
+        return new String[]{
+            " ****",
+            "**   ",
+            "**   ",
+            " *** ",
+            "   **",
+            "   **",
+            "**** "
+        };
+    }
+
+    // Main method
     public static void main(String[] args) {
 
-        
-        String[] lines = new String[7];
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-        // Populate array using String.join()
-        lines[0] = String.join(" ", " *** ",  " *** ",  " **** ", " ***** ");
-        lines[1] = String.join(" ", "*   *", "*   *", "*   *", "*     ");
-        lines[2] = String.join(" ", "*   *", "*   *", "*   *", "*     ");
-        lines[3] = String.join(" ", "*   *", "*   *", "**** ", " ***** ");
-        lines[4] = String.join(" ", "*   *", "*   *", "*     ", "     *");
-        lines[5] = String.join(" ", "*   *", "*   *", "*     ", "     *");
-        lines[6] = String.join(" ", " *** ",  " *** ",  "*     ", " ***** ");
-
-        
-        for (String line : lines) {
-            System.out.println(line);
+        // Print OOPS banner using loop
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                oPattern[i] + "  " +
+                oPattern[i] + "  " +
+                pPattern[i] + "  " +
+                sPattern[i]
+            );
         }
     }
 }
